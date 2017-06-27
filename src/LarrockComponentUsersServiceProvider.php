8c0bca5bd465fa43da3f565fe444d294afe0e264
Alexandr Fanamurov <fanamura@gmail.com>
2017-06-27 17:17:46 +1000

    Fix path

--- src/LarrockComponentUsersServiceProvider.php
+++ src/LarrockComponentUsersServiceProvider.php
@@ -35,7 +35,7 @@ class LarrockComponentUsersServiceProvider extends ServiceProvider
             $timestamp = date('Y_m_d_His', time());
 
             $this->publishes([
-                __DIR__.'/../database/migrations/0000_00_00_000000_create_users_table.php' => database_path('migrations/'.$timestamp.'_create_users_table.php')
+                __DIR__.'/database/migrations/0000_00_00_000000_create_users_table.php' => database_path('migrations/'.$timestamp.'_create_users_table.php')
             ], 'migrations');
         }
     }
