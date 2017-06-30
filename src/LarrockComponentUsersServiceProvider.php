d806c8bdbeb5153bedd5198bb98c35142b34c0b2
Alexandr Fanamurov <fanamura@gmail.com>
2017-06-30 18:03:53 +1000

    Fix

--- src/LarrockComponentUsersServiceProvider.php
+++ src/LarrockComponentUsersServiceProvider.php
@@ -16,7 +16,7 @@ class LarrockComponentUsersServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__.'/views', 'larrock');
 
         $this->publishes([
-            __DIR__.'/views' => base_path('resources/views/larrock'),
+            __DIR__.'/views' => base_path('resources/views/vendor/larrock')
         ]);
     }
 
