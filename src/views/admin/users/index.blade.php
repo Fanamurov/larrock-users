27596ad24bc464ca6a8630bed8cf0b41fb28ced8
Alexandr Fanamurov <fanamurov@ya.ru>
2018-03-05 17:17:07 +1000

    fix admin template

--- src/views/admin/users/index.blade.php
+++ src/views/admin/users/index.blade.php
@@ -3,22 +3,19 @@
 
 @section('content')
     <div class="container-head uk-margin-bottom">
-        <div class="add-panel uk-margin-bottom uk-text-right">
-            <a class="uk-button" href="#modal-help" data-uk-modal="{target:'#modal-help'}"><i class="uk-icon-question"></i></a>
-            <a class="uk-button uk-button-primary" href="/admin/{{ $app->name }}/create">Добавить пользователя</a>
-        </div>
-        <div id="modal-help" class="uk-modal">
-            <div class="uk-modal-dialog">
-                <a class="uk-modal-close uk-close"></a>
-                <p>{{ $app->description }}</p>
+        <div class="uk-grid">
+            <div class="uk-width-expand">
+                {!! Breadcrumbs::render('admin.'. $app->name .'.index') !!}
+            </div>
+            <div class="uk-width-auto">
+                @if(isset($allowCreate))
+                    <a class="uk-button uk-button-primary" href="/admin/{{ $app->name }}/create">Добавить пользователя</a>
+                @endif
             </div>
         </div>
-        <div class="uk-clearfix"></div>
-        {!! Breadcrumbs::render('admin.'. $app->name .'.index') !!}
-        <div class="uk-clearfix"></div>
     </div>
 
-    <div class="uk-margin-large-bottom">
+    <div class="uk-margin-large-bottom ibox-content">
         <table class="uk-table uk-table-striped uk-form">
             <thead>
             <tr>
@@ -43,7 +40,7 @@
                     <td class="row-id">{{ $data_value->id }}</td>
                     @foreach($app->rows as $row)
                         @if($row->in_table_admin_ajax_editable)
-                            @if(get_class($row) === 'Larrock\Core\Helpers\FormBuilder\FormCheckbox')
+                            @if($row instanceof \Larrock\Core\Helpers\FormBuilder\FormCheckbox)
                                 <td class="row-active @if($row->name !== 'active') uk-hidden-small @endif">
                                     <div class="uk-button-group btn-group_switch_ajax" role="group" style="width: 100%">
                                         <button type="button" class="uk-button uk-button-primary uk-button-small @if($data_value->{$row->name} === 0) uk-button-outline @endif"
@@ -54,7 +51,7 @@
                                                 data-row="active" data-value="0" style="width: 50%">off</button>
                                     </div>
                                 </td>
-                            @elseif(get_class($row) === 'Larrock\Core\FormBuilder\FormInput')
+                            @elseif($row instanceof \Larrock\Core\Helpers\FormBuilder\FormInput)
                                 <td class="uk-hidden-small">
                                     <input type="text" value="{{ $data_value->{$row->name} }}" name="{{ $row->name }}"
                                            class="ajax_edit_row form-control" data-row_where="id" data-value_where="{{ $data_value->id }}"
@@ -64,31 +61,35 @@
                         @endif
                         @if($row->in_table_admin)
                             <td class="uk-hidden-small">
-                                {{ $data_value->{$row->name} }}
+                                @if($row->name === 'email')
+                                    <a href="/admin/users/{{ $data_value->id }}/edit">{{ $data_value->{$row->name} }}</a>
+                                @else
+                                    {{ $data_value->{$row->name} }}
+                                @endif
                             </td>
                         @endif
                     @endforeach
                     @if($enable_cart)
                         <td>
-                            {{ count($data_value->cart) }}
+                            {{ \count($data_value->cart) }}
                         </td>
                         <td>
                             @php($cost = 0)
                             @foreach($data_value->cart as $order)
                                 @php($cost += $order->cost)
                             @endforeach
-                            <a target="_blank" href="/admin/search?text={{ $data_value->email }}">{{ $cost }} руб.</a>
+                            <a data-uk-tooltip title="Перейти к заказам пользователя" target="_blank" href="/admin/cart?user_search={{ $data_value->email }}">{{ $cost }} руб.</a>
                         </td>
                     @endif
                     <td>
-                        @if(count($data_value->role) > 0)
-                            <span class="uk-badge">{{ $data_value->role->first()->slug }}</span>
+                        @if(\count($data_value->role) > 0)
+                            <span class="uk-label">{{ $data_value->role->first()->slug }}</span>
                         @else
-                            <span class="uk-badge uk-badge-danger">Роль не назначена!</span>
+                            <span class="uk-label uk-label-danger">Роль не назначена!</span>
                         @endif
                     </td>
                     <td>
-                        <a href="/admin/users/{{ $data_value->id }}/edit" class="uk-button uk-button-small">Свойства</a>
+                        <a href="/admin/users/{{ $data_value->id }}/edit" class="uk-button uk-button-default uk-button-small">Свойства</a>
                     </td>
                     <td>
                         <form action="/admin/users/{{ $data_value->id }}" method="post">
@@ -101,6 +102,6 @@
             @endforeach
             </tbody>
         </table>
-        {!! $data->render() !!}
+        {!! $data->links('larrock::admin.pagination.uikit3') !!}
     </div>
 @endsection
